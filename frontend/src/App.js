a561d0ad099582e5630c3b706e1075545ec79010
Huang096 <huang.zhheng@gmail.com>
2025-04-04 21:13:25 +0800

    implemented basic project structure and header component

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* 这里放其它页面内容 */}
+    </div>
+  );
+}
+
+export default App;
